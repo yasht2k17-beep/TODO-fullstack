8f97f7c617d11ba66066abfddf9bcc726757e6a7
Yash <yasht2k17@gmail.com>
2026-02-25 16:47:54 +0530

    Moved DB credentials to env

--- back/database.py
+++ back/database.py
@@ -1,8 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
+import os
+from dotenv import load_dotenv
 
-database_url="postgresql://postgres:#Ty_5884@localhost:5432/todo"
+load_dotenv()
 
-engine=create_engine(database_url)
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine=create_engine(DATABASE_URL)
+SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+Base=declarative_base()
+engine=create_engine(DATABASE_URL)
 sessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 base=declarative_base()
